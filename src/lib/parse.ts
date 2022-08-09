5d2561748065f7d5198ab5aeba4a7c8f951e4ac4
Maxim Kosterov <maxim.kosterov@gmail.com>
2022-08-09 20:58:35 +0300

    Modified parser for flexibility

--- src/lib/parse.ts
+++ src/lib/parse.ts
@@ -1,41 +1,4 @@
-export const FIELDS = [
-  "Start Time",
-  "Time Completed",
-  "Elapsed Time",
-  "Sample ID",
-  "Experiment",
-  "Data File",
-  "Plot File",
-  "VSM Exp File",
-  "VSM Data File",
-];
-
-const enumeratedFields = (() => {
-  let i = 0;
-  let ret = [];
-
-  FIELDS.forEach((s) => {
-    i += 1;
-    ret.push([s, i]);
-  });
-  return ret;
-})();
-
-const metaregex = new RegExp(
-  enumeratedFields.map(([s, _]) => `${s}:\\s*(.*?)\\s*`).join("") +
-    "\\*\\*\\*DATA\\*\\*\\*",
-  "gm"
-);
-
-const unitregex = new RegExp(
-  "^Field\\((.*?)\\)\\s*Moment\\((.*?)\\)\\s*$",
-  "gm"
-);
-
-const dataregex = new RegExp(
-  "^\\s*([-\\.0123456789]+)\\s*([-\\.0123456789]+)\\s*$",
-  "gm"
-);
+import { parseLS } from "./parse/LS";
 
 export type XUnits = "Oe" | "A/m" | "T";
 export type YUnits = "emu" | "Am2";
@@ -64,92 +27,10 @@ export const TY_SUFFIX: Record<FileType, string> = {
 export const FILE_TYPES = Object.keys(TY_SUFFIX) as FileType[];
 
 export const parseFile = (source: string, ty: FileType): ParseResult | null => {
-  let meta = [];
-
-  const metamatch = [...source.matchAll(metaregex)][0];
-
-  if (metamatch == null || metamatch.length != 10) {
-    return null;
+  switch (ty) {
+    case FileType.LS_DCD:
+    case FileType.LS_IRM:
+    case FileType.LS_HYST:
+      return parseLS(source, ty);
   }
-
-  enumeratedFields.map(([s, i]) => {
-    meta.push([s, metamatch[i]]);
-  });
-
-  const datamatch = [...source.matchAll(dataregex)];
-
-  if (datamatch == null) {
-    return null;
-  }
-
-  const dataRead = datamatch.map((m) => {
-    return [parseFloat(m[1].trim()), parseFloat(m[2].trim())] as [
-      number,
-      number
-    ];
-  });
-
-  const data = (() => {
-    switch (ty) {
-      // DCD
-      case FileType.LS_DCD:
-        return cleanData(dataRead);
-      case FileType.LS_IRM:
-        return cleanData(dataRead);
-      case FileType.LS_HYST:
-        return dataRead;
-    }
-  })();
-
-  const unitmatch = [...source.matchAll(unitregex)][0];
-
-  if (unitmatch == null) {
-    return null;
-  }
-
-  const units = [unitmatch[1], unitmatch[2]] as [XUnits, YUnits];
-
-  return {
-    meta,
-    data,
-    ty,
-    units,
-    initUnits: units,
-    normalization: [null, null],
-  };
-};
-
-const isz = (x: number) => Math.abs(x) < 1;
-
-const cleanData = (read: [number, number][]): number[][] => {
-  let i = 0;
-
-  if (!isz(read[0][0])) {
-    i += 1;
-  }
-
-  let res = [];
-
-  if (isz(read[i][0])) {
-    res.push([read[i][0], read[i][1], read[i][1]]);
-    i += 1;
-  } else {
-    res.push([0, 0, 0]);
-  }
-
-  while (i < read.length) {
-    if (!isz(read[i][0])) {
-      if (isz(read[i + 1][0])) {
-        res.push([read[i][0], read[i][1], read[i + 1][1]]);
-        i += 2;
-      } else {
-        res.push([read[i][0], read[i][1], null]);
-        i += 1;
-      }
-    } else {
-      i += 2;
-    }
-  }
-
-  return res;
 };
---
+++ src/lib/parse/LS.ts
@@ -0,0 +1,131 @@
+import { FileType, ParseResult, XUnits, YUnits } from "../parse";
+
+const FIELDS = [
+  "Start Time",
+  "Time Completed",
+  "Elapsed Time",
+  "Sample ID",
+  "Experiment",
+  "Data File",
+  "Plot File",
+  "VSM Exp File",
+  "VSM Data File",
+];
+
+const enumeratedFields = (() => {
+  let i = 0;
+  let ret = [];
+
+  FIELDS.forEach((s) => {
+    i += 1;
+    ret.push([s, i]);
+  });
+  return ret;
+})();
+
+const metaregex = new RegExp(
+  enumeratedFields.map(([s, _]) => `${s}:\\s*(.*?)\\s*`).join("") +
+    "\\*\\*\\*DATA\\*\\*\\*",
+  "gm"
+);
+
+const unitregex = new RegExp(
+  "^Field\\((.*?)\\)\\s*Moment\\((.*?)\\)\\s*$",
+  "gm"
+);
+
+const dataregex = new RegExp(
+  "^\\s*([-\\.0123456789]+)\\s*([-\\.0123456789]+)\\s*$",
+  "gm"
+);
+
+export const parseLS = (source: string, ty: FileType): ParseResult | null => {
+  let meta = [];
+
+  const metamatch = [...source.matchAll(metaregex)][0];
+
+  if (metamatch == null || metamatch.length != 10) {
+    return null;
+  }
+
+  enumeratedFields.map(([s, i]) => {
+    meta.push([s, metamatch[i]]);
+  });
+
+  const datamatch = [...source.matchAll(dataregex)];
+
+  if (datamatch == null) {
+    return null;
+  }
+
+  const dataRead = datamatch.map((m) => {
+    return [parseFloat(m[1].trim()), parseFloat(m[2].trim())] as [
+      number,
+      number
+    ];
+  });
+
+  const data = (() => {
+    switch (ty) {
+      // DCD
+      case FileType.LS_DCD:
+        return cleanData(dataRead);
+      case FileType.LS_IRM:
+        return cleanData(dataRead);
+      case FileType.LS_HYST:
+        return dataRead;
+    }
+  })();
+
+  const unitmatch = [...source.matchAll(unitregex)][0];
+
+  if (unitmatch == null) {
+    return null;
+  }
+
+  const units = [unitmatch[1], unitmatch[2]] as [XUnits, YUnits];
+
+  return {
+    meta,
+    data,
+    ty,
+    units,
+    initUnits: units,
+    normalization: [null, null],
+  };
+};
+
+const isz = (x: number) => Math.abs(x) < 1;
+
+const cleanData = (read: [number, number][]): number[][] => {
+  let i = 0;
+
+  if (!isz(read[0][0])) {
+    i += 1;
+  }
+
+  let res = [];
+
+  if (isz(read[i][0])) {
+    res.push([read[i][0], read[i][1], read[i][1]]);
+    i += 1;
+  } else {
+    res.push([0, 0, 0]);
+  }
+
+  while (i < read.length) {
+    if (!isz(read[i][0])) {
+      if (isz(read[i + 1][0])) {
+        res.push([read[i][0], read[i][1], read[i + 1][1]]);
+        i += 2;
+      } else {
+        res.push([read[i][0], read[i][1], null]);
+        i += 1;
+      }
+    } else {
+      i += 2;
+    }
+  }
+
+  return res;
+};
